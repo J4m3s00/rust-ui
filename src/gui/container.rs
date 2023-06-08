20c7c0214e33ed438f6f4e53c8cb0562739a3c85
Luca <james.lol2000@gmail.com>
2023-06-09 01:52:48 +0200

    start work on correct container

--- src/gui/container.rs
+++ src/gui/container.rs
@@ -23,32 +23,10 @@ impl ContainerItem {
         self.size = size;
         self
     }
-}
 
-#[derive(Default)]
-pub struct Container {
-    pub children: Vec<ContainerItem>,
-}
-
-impl Container {
-    pub fn add_child<T>(&mut self, child: T) -> &mut ContainerItem
-    where
-        T: Widget + 'static,
-    {
-        self.children.push(ContainerItem {
-            widget: Box::new(child),
-            size: SizePolicy::Percentage(1.0).into(),
-        });
-        self.children.last_mut().expect("Failed to add child")
+    pub fn widget(&self) -> &dyn Widget {
+        &*self.widget
     }
 }
 
-impl Widget for Container {
-    fn build(&self, push_child: &mut WidgetBuilder, size: SizePolicy2D) {
-        let mut composer = push_child.child(size);
-
-        for item in self.children.iter() {
-            composer = composer.widget(item.widget.as_ref(), item.size);
-        }
-    }
-}
+pub trait Container: Widget {}
